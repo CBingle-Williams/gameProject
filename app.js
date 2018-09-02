3cf548bf6dc697dcf51f3426a69bbc72656785b8
CBingle-Williams <cbingle-williams@spartaglobal.com>
2018-09-02 17:03:06 +0100

    updated images, implemented killBird feature

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ var CANVAS_HEIGHT = removePX(window.getComputedStyle(document.getElementsByClass
 var CANVAS_WIDTH = removePX(window.getComputedStyle(document.getElementsByClassName('container')[0], null).getPropertyValue("width"));
 var FREE_SPACE = 200;
 var pipe = document.getElementById('first');
+var bird = document.getElementById('bird');
+var square = document.getElementsByClassName('square')[0];
 var first = true;
+var collision = false;
 var counter = 0;
 
 function pipes(id) {
     var elem = document.getElementById(id);
-    var position = elem.getElementsByClassName('parent')[0];
     var topElem = elem.getElementsByClassName('box')[0];
     var bottomElem = elem.getElementsByClassName('box')[1];
-    //var squares = document.getElementsByClassName('square')[0];
     var space = [220,420];
     var pos = 0;
-    var id = setInterval(frame, 10);
+    var id = setInterval(frame, 6);
     function frame() {
+        if (collision){clearInterval(id)};
         if(first && pos == 290){
             pipes('second');
             first = false;
@@ -28,25 +30,44 @@ function pipes(id) {
         space[1] = CANVAS_HEIGHT - bottom;
         pos = 0;
       } else {
-            checkCollisions(pos, space[0], space[1]);
+            if(checkCollisions(pos, space[0], space[1])){collision = true};
             pos++;
             elem.style.right = pos + 'px';
         }
     }
 };
 
+function killBird() {
+    bird.innerHTML = '<img src="bird1.png" height="50px" width="50px"></img>';
+    square.style.transform = 'rotate(+90deg)';
+    var height = removePX(window.getComputedStyle(square, null).getPropertyValue("top"));
+    var counter = height;
+    let id = setInterval(drop,0.01);
+    function drop() {
+        if (removePX(window.getComputedStyle(square, null).getPropertyValue("top")) > 590) {
+            clearInterval(id);
+            return 0;
+        }
+        else {
+            console.log('hello');
+            counter = counter+4;
+            square.style.top = counter +'px';
+        }
+    }
+}
+
 function checkCollisions(x_axis, top, bottom){
-    console.log(x_axis + " " + top + " " + bottom);
     if(x_axis == 400){
-        if (!(300 > top && bottom > (300+50))){
-            alert('collision');
+        if (!(100 > top && bottom > (100+50))){
+            killBird();
+            return true;
         }
         else {
             counter++;
             document.getElementById('counter').innerHTML = counter;
         }
     }
-}
+};
 
 function removePX(string) {
     return parseInt(string.replace('px',''));
